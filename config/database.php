d58c8c6e446931b3ea6e42f8b38d13f9a7bad4d4
Alexis <alexis.guermann@edu.ece.fr>
2026-05-31 16:22:47 +0200

    Correction de la connexion à la base de données locale

--- config/database.php
+++ config/database.php
@@ -10,9 +10,9 @@ function get_pdo_connection()
 
     try {
         $pdo = new PDO(
-            "mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=voyagevista;charset=utf8mb4",
+            "mysql:host=127.0.0.1;port=3307;dbname=voyagevista;charset=utf8mb4",
             "root",
-            "root",
+            "",
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
